dded80f24ddea537d2e400550705daeb63f13426
Náyade Velasco Acosta <naya@MacBook-Air-de-Nayade.local>
2025-10-07 19:20:29 -0600

    Guardando mis cambios locales antes de merge

---
+++ students/Nayavvv/intro_python/hola.py
@@ -0,0 +1,11 @@
+# 1. Un print() con un saludo
+print("¡Hola mundo!")
+
+#2 Una variable con resultado 
+suma = 2 + 10 
+print ("2 + 10 es igual a:", suma)
+
+#3 Funcion simple y mostrar su uso 
+def cuadrado(x):
+	return x * x
+print ("el cuadrado de 4 es:", cuadrado(4))
