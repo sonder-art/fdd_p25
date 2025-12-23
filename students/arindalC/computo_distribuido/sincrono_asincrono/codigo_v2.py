fa0a57689ba691cb4e942ba3a2d0478f64b4e7bf
arindalC <arindal.contreras@itam.mx>
2025-12-23 09:08:45 -0600

    Entrega tareas arindalC

---
+++ students/arindalC/computo_distribuido/sincrono_asincrono/codigo_v2.py
@@ -0,0 +1,7 @@
+import asyncio
+
+# Esto es una corrutina (función asíncrona)
+async def tarea_simple():
+    print("Inicio de la tarea")
+    await asyncio.sleep(1)  # Simula una espera (wait)
+    print("Fin de la tarea")
---
+++ students/arindalC/intro_python/hola.py
@@ -0,0 +1,11 @@
+# 1) Un print con un saludo o mensaje
+print("Hola, probando el script hola.py")
+
+# 2) Una variable con una operación aritmética y un print de su resultado
+resultado = 5 * 8
+print("El resultado de 5 * 8 es:", resultado)
+
+# 3) Una función simple y un print de su llamada
+def cuadrado(x):
+    return x ** 2
+print("El cuadrado de 6 es:", cuadrado(6))
